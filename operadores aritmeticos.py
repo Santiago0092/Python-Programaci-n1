bf6b02c16e3b5186b60faecd777a1a14410ddd6e
Santiago Monroy <johandmonroyAunisangil.edu.co>
2025-02-26 16:06:48 -0500

    operadores aritmeticos

---
+++ operadores aritmeticos.py
@@ -0,0 +1,9 @@
+#libreria
+import math
+#operadores aritmeticos
+#ejericio 1
+ejemplo1 = 10 + 2*(-3 * ((7 - 2) + (7 * (3 - 2))))
+print(ejemplo1)
+#ejericio 2
+ejemplo2 = 2*(4*((7+4*(5*3-9))) -3*(40-8))
+print(ejemplo2)
