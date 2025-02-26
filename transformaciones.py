5c9deab9a9dcd0acd2e68487b1491b6c8d444f67
Santiago Monroy <johandmonroyAunisangil.edu.co>
2025-02-26 14:59:25 -0500

    transformaciones de variables

---
+++ transformaciones.py
@@ -0,0 +1,13 @@
+nombre = input("Ingrese su nombre: ")
+print(nombre)
+print(type(nombre))
+edad = int (input("Ingrese su edad: "))
+print(edad)
+print(type(edad))
+#estado de matricula
+estado = bool(input("Ingrese su estado de matricula: true/false "))
+print(estado)
+print(type(estado))
+peso = float(input("Ingrese su peso: "))
+print(peso)
+print(type(peso))
