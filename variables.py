4480da36fb6fa404961c68e694a09c890e181247
Santiago Monroy <johandmonroyAunisangil.edu.co>
2025-02-26 14:41:56 -0500

    tipos de variables

---
+++ variables.py
@@ -0,0 +1,17 @@
+entero = 100
+print(type(entero))
+decimal = 29.2
+print(type(decimal))
+letra = 'A'
+print(type(letra))
+mensaje = "Hola Mundo"
+print(type(mensaje))
+booleano = True
+print(type(booleano))
+booleano = False
+print(type(booleano))
+parrafo = """
+Estudiante: Johand Santiago Monroy Cortés
+Correo: johandmonroy224@unisangil.edu.co
+"""
+print(type(parrafo))
