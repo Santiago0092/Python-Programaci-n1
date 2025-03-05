d5abfe1a7ae4ca67251737abae5efa0ee1e84fd5
Santiago Monroy <johandmonroyAunisangil.edu.co>
2025-03-05 16:18:21 -0500

    usuario y contraseña

---
+++ Inicio_de_sesión.py
@@ -0,0 +1,8 @@
+Santiago = "johandMonroy"
+password = 1234
+print(type(password))
+
+usuario= input("Usuario: ")
+contraseña = int(input("Ingrese su contraseña: "))
+print(type(contraseña))
+print(Santiago == usuario and password == contraseña)
