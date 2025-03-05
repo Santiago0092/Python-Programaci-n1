6cd5f6daeb9fbc48120d800a2ba26c5f1300cafe
Santiago Monroy <johandmonroyAunisangil.edu.co>
2025-03-05 15:19:49 -0500

    terrenos

---
+++ terreno.py
@@ -0,0 +1,17 @@
+import math
+#terreno
+base = float(input("ingrese la base del terreno: "))
+Altura = float(input("ingrese la altura del terreno - triangulo: "))
+Altura2 = float(input("ingrese la altura del terreno - rectangulo: "))
+lado = float(input("ingrese el lado del terreno: "))
+#operaciones 
+Triangulo= (base*Altura)/2
+print(f"el area del triangulo es: {Triangulo}")
+Rectangulo= base*Altura2
+print(f"el area del rectangulo es: {Rectangulo}")
+area_total= Triangulo+Rectangulo
+print(f"el area total del terreno es: {area_total}")
+radio= float(input("ingrese el radio del terreno- circular: "))
+Circulo= math.pi*(math.pow(radio**2))
+print(f"el area del circulo es: {Circulo}")
+
