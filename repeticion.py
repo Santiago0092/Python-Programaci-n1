87cdc2835a2851ceb6691cfe84b9d5f1d15aa0fd
Santiago Monroy <johandmonroyAunisangil.edu.co>
2025-03-12 14:53:12 -0500

    Calculadora

---
+++ repeticion.py
@@ -0,0 +1,17 @@
+opcion = input("Ingrese una opción: ")
+#while
+while opcion != "salir":
+    print("dentro del bucle")
+    opcion = input("Ingrese una opción: ")
+print("fuera del bucle")
+
+#while True
+while True:
+    print("dentro del bucle")
+    opcion = input("Ingrese una opción: ")
+    if opcion == "salir":
+        break
+print("fuera del bucle")
+   
+
+    
