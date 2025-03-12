0e75f574097d93f9b5c4e4825f45077532e8af4f
Santiago Monroy <johandmonroyAunisangil.edu.co>
2025-03-12 14:31:32 -0500

    Control

---
+++ estructurasControl.py
@@ -0,0 +1,16 @@
+
+
+
+numero =int(input("Ingrese un número: "))
+print ("el numero es: ", numero)
+if numero > 4:
+    print("El número es mayor a 5")
+    print("fuera de la estructura de control")
+else:
+    print ("El número es menor a 5")
+
+#Ejemplo 2
+if numero == 3:
+  print ("el numero es igual a 3")
+else:
+   print("error")
