87ae273385c777448076accd201bbae6ea897c85
Santiago Monroy <johandmonroyAunisangil.edu.co>
2025-04-02 14:28:25 -0500

    cicloFor

---
+++ cicloFor.py
@@ -0,0 +1,15 @@
+# Ciclo for
+for i in range(1, 10, 2):
+    print(i)
+
+x = "Ayuda"
+
+#separar caracteres
+for arroz in x:
+    if arroz == "d":
+        break
+    else:
+       print(arroz)
+       #tabala de multiplicar
+for mul in range(1, 11):
+    print(f"2 x {mul} = {2*mul}")
