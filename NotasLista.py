5b3db197f85222633caf98db273ca382c868d9ba
Santiago Monroy <johandmonroyAunisangil.edu.co>
2025-04-02 16:03:12 -0500

    Listas

---
+++ NotasLista.py
@@ -0,0 +1,11 @@
+lista = []
+# Preguntar datos para agregar a la lista
+for i in range(8):
+        materia = input("Ingrese la materia: ") 
+        nota = float(input("Ingrese la nota: "))
+        # Agregar materia y nota a la lista
+        lista.append((materia, nota))    
+print("Lista final:", lista)
+
+for dato in lista:
+    print(f"dato: {dato}")
--- listas.py
+++ listas.py
@@ -43,4 +43,30 @@ Numeros.append(1000)
 Numeros.insert(5, "Celular")
 Numeros.insert(3, "Crema")
 print(Numeros)
+#Eliminar valores de la lista
+Numeros.remove(100)
+print(Numeros)
+Numeros.remove("Crema")
+print(Numeros)
 print(f"el tamaño de la lista es: {len(Numeros)}")
+#Eliminar el último elemento de la lista
+Numeros.pop()
+print(Numeros)
+print(f"el tamaño de la lista es: {len(Numeros)}")
+Numeros [2:4] = []
+print(Numeros)
+print(f"el tamaño de la lista es: {len(Numeros)}")
+
+for dato in Numeros:
+    print(f"dato: {dato}")
+
+
+Lista1 = [1, 10, 50, 20, 1000]
+print(Lista1)
+Lista1.reverse()
+print(Lista1)
+Lista1.sort()
+print(Lista1)
+Lista1.sort(reverse=True)
+print(Lista1)
+
