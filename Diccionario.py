6466b8b22c62b76d03f659c350cdc330306c78e0
Santiago Monroy <johandmonroyAunisangil.edu.co>
2025-04-09 15:45:54 -0500

    Diccionario

---
+++ Diccionario.py
@@ -0,0 +1,64 @@
+estudiantes = {
+        "Estudiante": [
+        {
+        "Nombre": "José",
+        "apellido": "Moreno",
+        "cedula": 3123222131}],
+
+        "estudiante2": [{
+        "Nombre": "Tomás",
+        "apellido": "Merchán",
+        "celular": 3121231}],
+
+        "estudiante3": [{
+        "Nombre": "Haider",
+        "apellido": "Machado",
+        "celular": 103222131}]
+
+        
+   }
+   
+
+
+
+Diccionario = {
+    "Nombre": "santiago",
+    "celular": 31232131,
+    "universidad": "unisangil",
+    "estado": True,  # Corrected to a boolean
+    "Estatura": 1.70,
+    "Edad": 18,
+    "lista": ["1", "2", "3", "4", "5"]
+}
+
+# Tipo de dato
+
+
+
+
+programacion ={
+ 
+    "Nombre": "santiago",
+    "apellido": "Monroy",
+    "celular": 31232131,
+    "universidad": "unisangil",
+    "correo": "santiago@gmail.com",
+    "estado": "True",
+    "Estatura": 1.70,
+    "Edad": 18,
+    "lista": ["1", "2", "3", "4", "5"],
+    "direccion": "calle 22A #4-33 urb primavera",
+    "Lista2": ["Arroz", "Frijoles", "Lentejas"]
+}
+
+# Acceder a los valores del diccionario
+print(Diccionario)
+print(programacion)
+print(programacion["Nombre"])
+
+for estudiante in estudiantes["Estudiante"]:
+    print(estudiante["Nombre"])
+    print(estudiante["apellido"])
+    print(estudiante["cedula"])
+
+    print(programacion.items())
--- NotasLista.py
+++ NotasLista.py
@@ -1,10 +1,9 @@
 lista = []
-# Preguntar datos para agregar a la lista
 for i in range(8):
         materia = input("Ingrese la materia: ") 
         nota = float(input("Ingrese la nota: "))
         # Agregar materia y nota a la lista
-        lista.append((materia, nota))    
+        lista.append(materia, nota)    
 print("Lista final:", lista)
 
 for dato in lista:
